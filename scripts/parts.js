b4094df0e29412109c25bca05187c0a272c799b3
Filipe Maia <fme.maia@gmail.com>
2020-11-19 11:40:39 +0000

    1st commit

---
+++ scripts/parts.js
@@ -0,0 +1,33 @@
+
+let parts = [];
+
+class part {
+	
+	constructor(kind, maker, model, imgUrl) {
+		this.kind = kind;
+		this.maker=maker;
+		this.model=model;
+		this.imgUrl = imgUrl;
+	}
+	
+	print = (tableId) => {
+		partsTable = document.getElementById(tableId);
+		for (let i = 0; i < 4; i++) {
+			let newTr = document.createElement('tr');
+			newTr.innerHTML = '<td>$(kind)</td><td>$(maker)</td><td>model</td><td><img src="$(imgUrl)" /></td>';
+			partsTable.appendChild(newTr);
+		}
+	}
+}
+
+let voodoo = new part('Video Card', '3dfx', 'Voodoo', 'https://tpucdn.com/gpu-specs/images/c/3570-front.jpg');
+let soundblaster = new part('Sound/Game Card', 'Creative', 'SoundBlaster', 'https://i.ebayimg.com/images/g/um8AAOSw7speQjQ1/s-l640.jpg');
+
+parts[0] = voodoo;
+parts[1] = soundblaster;
+
+function printParts(tableId) {
+	for (let i = 0; i < parts.length; i++) {
+		parts[i].print(tableId);
+	}
+}
